2bbaa9e956bd1a6b8dc2f2c62995d7ba39f7382c
AbdelrahmanElhussiny <arahman1783@yahoo.com>
2025-02-17 17:55:36 +0200

    feat/ check out element

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,12 +1,17 @@
 import { Heading } from '../../components/Heading'
 import style from './Checkout.module.css'
+import CartElement from './components/CartElement/CartElement'
 
 
 export default function Checkout() {
   return (
     <div className={style.container}>
       <Heading title="Items selected to" underlinedTxt = "checkout" />
-      
+      <CartElement 
+          image="https://images.unsplash.com/photo-1502462041640-b3d7e50d0662?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          productName="Vanilla Latte"
+          price={20}
+          />
     </div>
   )
 }
---
+++ src/pages/checkout/components/CartElement/CartElement.jsx
@@ -0,0 +1,18 @@
+import style from './CartElement.module.css'
+
+export default function CartElement({image, productName, price}) {
+  return (
+    <div className={style.container}>
+      <div className={style.imgContainer}>
+        <img src={image} alt={productName}/>
+      </div>
+      <div className={style.productName}>{productName}</div>
+      <div className={style.price}>{price}</div>
+      <div className={style.counter}>
+        +
+        0
+        -
+        </div>
+    </div>
+  )
+}
